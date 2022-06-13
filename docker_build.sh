00c7d987e37519d31d57cae3fb4faffc9cd1eb20
AndreyCodes <aa2503@bk.ru>
2022-06-13 19:20:50 +0300

    4

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
 cd /Ansa
-mkdir build
+mkdir build || exit 1
 cd build
 cmake .. || exit 1
 cmake --build . || exit 1
 ctest . || exit 1
 cd ..
-tar -zcvf bin.tar.gz ./build/bin 
+tar -zcvf bin.tar.gz ./build/bin || exit 1 
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
-podman build -t fallow_engine . 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-podman exec \
-  --name fallow_builder \
+podman build -t fallow_builder ${SCRIPT_DIR} 
+
+podman run \
   --mount type=bind,source=${WORKSPACE_DIR}/,target=/Ansa \
-  /bin/bash /docker_builder.sh
+  fallow_builder \
+  /bin/bash /docker_build.sh
