334207470b289ce58ebf3b5fdfb942c601f56ecb
AndreyCodes <aa2503@bk.ru>
2022-06-13 18:33:33 +0300

    2

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 podman build -t fallow_engine . 
 
-podman run \
+podman exec \
   --name fallow_builder \
   --mount type=bind,source=${WORKSPACE_DIR}/,target=/Ansa \
   /bin/bash /docker_builder.sh
